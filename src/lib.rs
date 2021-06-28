f6783f904b507f9a77dc5c53c55e47281e9b5fed
nyanpasu64 <nyanpasu64@tuta.io>
2021-06-28 16:12:58 -0700

    Rename Ptr to AliasPtr

--- src/lib.rs
+++ src/lib.rs
@@ -6,30 +6,30 @@ use std::ptr::NonNull;
 /// Only necessary until http://blog.pnkfx.org/blog/2021/03/25/how-to-dismantle-an-atomic-bomb/ is fixed,
 /// at which point we can use &UnsafeCell<T> instead.
 #[repr(transparent)]
-pub struct Ptr<T: ?Sized>(NonNull<T>);
+pub struct AliasPtr<T: ?Sized>(NonNull<T>);
 // PhantomData is not necessary to prevent leaking Box<&'stack U> variables.
 // Also read https://docs.rs/crate/ptr/0.2.2/source/src/lib.rs for reference.
 
-impl<T: ?Sized> Clone for Ptr<T> {
+impl<T: ?Sized> Clone for AliasPtr<T> {
     fn clone(&self) -> Self {
         Self(self.0)
     }
 }
 
-impl<T: ?Sized> From<Box<T>> for Ptr<T> {
+impl<T: ?Sized> From<Box<T>> for AliasPtr<T> {
     fn from(item: Box<T>) -> Self {
         // Safety: pointer is obtained from Box::into_raw().
         unsafe { Self::from_raw(Box::into_raw(item)) }
     }
 }
 
-impl<T: Sized> Ptr<T> {
-    pub fn new(x: T) -> Ptr<T> {
-        Ptr::from(Box::new(x))
+impl<T: Sized> AliasPtr<T> {
+    pub fn new(x: T) -> AliasPtr<T> {
+        AliasPtr::from(Box::new(x))
     }
 }
 
-impl<T: ?Sized> Ptr<T> {
+impl<T: ?Sized> AliasPtr<T> {
     /// Requirements: p must be valid (its target is readable and writable).
     /// In order for calling delete() to be sound,
     /// p must be obtained from Box::into_raw().
@@ -46,7 +46,7 @@ impl<T: ?Sized> Ptr<T> {
         self.clone()
     }
 
-    /// Requirements: The Ptr must be derived from Box::into_raw(),
+    /// Requirements: The AliasPtr must be derived from Box::into_raw(),
     /// and neither self nor aliasing pointers can be safely dereferenced
     /// (a safe but unsound operation) after calling delete().
     ///
@@ -66,12 +66,12 @@ impl<T: ?Sized> Ptr<T> {
     }
 }
 
-impl<T: ?Sized> Deref for Ptr<T> {
+impl<T: ?Sized> Deref for AliasPtr<T> {
     type Target = T;
     fn deref(&self) -> &T {
-        // Safety: Ptr is always constructed from a Box,
+        // Safety: AliasPtr is always constructed from a Box,
         // so can be dereferenced safely.
-        // It is the responsibility of the user to never delete() a Ptr
+        // It is the responsibility of the user to never delete() a AliasPtr
         // then dereference it or its aliases afterwards.
         unsafe { &*self.0.as_ptr() }
     }
@@ -85,11 +85,11 @@ mod tests {
     use std::cell::Cell;
     use std::mem::size_of;
 
-    struct AliasedPair(Ptr<Cell<i32>>, Ptr<Cell<i32>>);
+    struct AliasedPair(AliasPtr<Cell<i32>>, AliasPtr<Cell<i32>>);
 
     impl AliasedPair {
         fn new(x: i32) -> AliasedPair {
-            let x = Ptr::new(Cell::new(x));
+            let x = AliasPtr::new(Cell::new(x));
             AliasedPair(x.copy(), x)
         }
     }
@@ -104,8 +104,8 @@ mod tests {
 
     #[test]
     fn test_option_size_of() {
-        assert_eq!(size_of::<usize>(), size_of::<Ptr<i32>>());
-        assert_eq!(size_of::<usize>(), size_of::<Option<Ptr<i32>>>());
+        assert_eq!(size_of::<usize>(), size_of::<AliasPtr<i32>>());
+        assert_eq!(size_of::<usize>(), size_of::<Option<AliasPtr<i32>>>());
     }
 
     #[test]
@@ -116,9 +116,9 @@ mod tests {
     }
 
     // /// Does not compile, as expected.
-    // fn f() -> Ptr<&'static i32> {
+    // fn f() -> AliasPtr<&'static i32> {
     //     let x = 1;
-    //     let out = Ptr::new(&x) as Ptr<&'static i32>;
+    //     let out = AliasPtr::new(&x) as AliasPtr<&'static i32>;
     //     out
     // }
 }
